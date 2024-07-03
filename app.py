262712e1134bf21da2729ef4d179fbfdc63fffd1
Reinjosh <147047698+Reinjosh@users.noreply.github.com>
2024-07-03 12:01:31 +0800

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
+!pip install Flask
 from flask import Flask, render_template, request
+import google.generativeai as palm
+
+palm.configure(api_key="AIzaSyA_lMI55z6gDdGSf4dyJmb_I8cMXgv3wT0")
+
+model = {"model": "models/chat-bison-001"}
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    r = request.form.get("q")
+    print(r)
+    return(render_template("main.html",r=r))
+
+@app.route("/traffic_thailand", methods=["GET", "POST"])
+def traffic_thailand():
+    q = "thailand traffic"
+    r = palm.chat(**model, messages=q)
+    return(render_template("traffic_thailand.html",r=r.last))
 
 if __name__ == "__main__":
     app.run()
